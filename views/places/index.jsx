29be2f6746db7c267c21238ea7951d84f2b619e8
vicentemorales <vmorales121@gmail.com>
2023-04-25 16:42:26 -0700

    set up index jsx(still need to reference places PENDING)

---
+++ views/places/index.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Def = require('./default')
+
+function index () {
+    return (
+      <Def>
+          <main>
+              <h1>INDEX</h1>
+          </main>
+      </Def>
+    )
+  }
+  
+
+module.exports = index
