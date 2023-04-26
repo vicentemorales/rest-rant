147e8a1442254683e9e75f116b0634f2cb3ad250
vicentemorales <vmorales121@gmail.com>
2023-04-25 19:42:01 -0700

    fixed the places js file

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,15 +1,25 @@
 const React = require('react')
-const Def = require('./default')
+const Def = require('../default')
 
-function index () {
+function index (data) {
+  let placesFormatted = data.places.map((place) => {
     return (
-      <Def>
-          <main>
-              <h1>INDEX</h1>
-          </main>
-      </Def>
+      <div>
+        <h2>{place.name}</h2>
+        <img src={place.pic} alt={place.name}/>
+      </div>
     )
-  }
+  })
+  return (
+    <Def>
+        <main>
+            <h1>PLACES INDEX PAGE</h1>
+            {placesFormatted}
+        </main>
+    </Def>
+)
+}
+
   
 
 module.exports = index
