d4c4e06f3045b6f8f996fca05579ee5357ebb2cd
vicentemorales <vmorales121@gmail.com>
2023-04-25 19:56:14 -0700

    added style.css to color h1

---
+++ views/default.jsx
@@ -0,0 +1,18 @@
+const React = require('react')
+
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+                <link rel="stylesheet" href="/css/style.css" />
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"></link>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+}
+
+module.exports = Def
