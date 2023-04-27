c4e99d35bb643eecc28d567153a88dfdc78fabde
vicentemorales <vmorales121@gmail.com>
2023-04-27 16:19:25 -0700

    adding and rendering new view

--- views/home.jsx
+++ views/home.jsx
@@ -6,17 +6,17 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
-              <div>
+              <div className='homeContainer'>
                 <img className="dragon" src="/images/dragonfruitbowl.jpg" alt="Dragon Fruit Bowl" />
                 <div>
                   Photo By: <a href="https://unsplash.com/@cravethebenefits">Brenda Godinez</a>
                    on <a href="unsplash.com">UNSPLASH</a>
                 </div>
-              </div>
+              
               <a href="/places">
                 <button className="btn-primary">Places Page</button>
               </a>
-
+              </div>
           </main>
       </Def>
     )
