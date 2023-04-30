acce5cb3545e42a857ebc6ce3d83661c1fec0d5d
vicentemorales <vmorales121@gmail.com>
2023-04-30 10:30:27 -0700

    added a edit(working) button

--- controllers/places.js
+++ controllers/places.js
@@ -49,7 +49,7 @@ router.get('/:id/edit', (req, res) => {
       res.render('error404')
   }
   else {
-    res.render('places/edit', { place: places[id] })
+    res.render('places/edit', { place: places[id] , id })
   }
 })
 
